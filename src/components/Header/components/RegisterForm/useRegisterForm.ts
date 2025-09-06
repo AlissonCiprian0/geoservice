14e75d69d11081f11bd0300a256e24181b135c93
Alisson Cipriano <alisson2752000@gmail.com>
2025-09-06 01:04:52 -0300

    feat: added automatic login after register success

--- src/components/Header/components/RegisterForm/useRegisterForm.ts
+++ src/components/Header/components/RegisterForm/useRegisterForm.ts
@@ -6,10 +6,14 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { formSchema } from './constants';
-import type { User } from '@/store/authStore';
+import { useAuthStore, type User } from '@/store/authStore';
 import { toast } from 'sonner';
+import { useNavigate } from 'react-router';
 
 const useRegisterForm = () => {
+  const navigate = useNavigate();
+  const { login } = useAuthStore();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -27,6 +31,26 @@ const useRegisterForm = () => {
     RegisterData
   >({
     mutationFn: UserService.register,
+    onSuccess: async (_data, variables) => {
+      try {
+        const authResponse = await UserService.login({
+          email: variables.email,
+          password: variables.password,
+        });
+
+        login(authResponse.user, authResponse.token);
+
+        navigate('/');
+      } catch (loginError) {
+        const error = loginError as AxiosError<ApiError>;
+        const errorMessage =
+          error.response?.data?.error ||
+          'Login automático falhou. Por favor, tente entrar manualmente.';
+
+        console.error('Falha no login automático após o registro:', loginError);
+        toast.error(errorMessage);
+      }
+    },
     onError: (error) => {
       console.log('Erro:', error.response?.data?.error || error.message);
       toast.error(error.response?.data?.error || error.message);
